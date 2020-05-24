1f12e8b37d658a44ca702708d0d87f43f7c0b66e
zyt <993287359@qq.com>
2020-05-24 13:21:19 +0800

    init repo

---
+++ src/test/java/zyt/test/comunity/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package zyt.test.comunity.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
